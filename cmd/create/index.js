09663876777df0fed38ace411c18853dba0053ee
masato nakatsuji <nakatsuji.sak19@gmail.com>
2023-12-21 00:12:40 +0900

    12/20

--- cmd/create/index.js
+++ cmd/create/index.js
@@ -1,6 +1,97 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const Cli_1 = require("@flagfw/flag/bin/Cli");
-exports.default = () => {
-    Cli_1.default.outn("create server");
-};
+const Util_1 = require("@flagfw/server/bin/common/Util");
+exports.default = () => __awaiter(void 0, void 0, void 0, function* () {
+    Cli_1.default.outn("create server").br().indent(2);
+    let server = {};
+    let name;
+    while (!name) {
+        name = (yield Cli_1.default.in("Q. Enter server name.")).toString();
+        if (!name) {
+            Cli_1.default.red("[ERROR] ").outn("No server name was entered. Please retry.");
+            continue;
+        }
+        const already = Util_1.default.getServer(name);
+        if (already) {
+            Cli_1.default.red("[ERROR] ").outn("The same server name already exists. Please specify a different name.");
+            name = null;
+            continue;
+        }
+    }
+    server.name = name;
+    // host name
+    let host = (yield Cli_1.default.in("Q. Enter host name (domain). (*)")).toString();
+    if (!host) {
+        host = "*";
+    }
+    server.host = host;
+    // ssl
+    let ssl = yield Cli_1.default.in("Q. Do you want to make an SSL connection? [y/n] (y)");
+    server.ssl = false;
+    if (!ssl ||
+        ssl == "y" ||
+        ssl == "Y") {
+        server.ssl = true;
+    }
+    if (server.ssl) {
+        Cli_1.default.indent(4);
+        // ssl key
+        const defaultSslKey = Util_1.default.getHome() + "/" + server.name + "/server.key";
+        let sslKey = (yield Cli_1.default.in("Q. Specify the private key path of the server certificate. (" + defaultSslKey + ")")).toString();
+        if (!sslKey) {
+            sslKey = defaultSslKey;
+        }
+        server.sslKey = sslKey;
+        // ssl cert
+        const defaultSslCert = Util_1.default.getHome() + "/" + server.name + "/server.crt";
+        let sslCert = (yield Cli_1.default.in("Q. Specify server certificate path. (" + defaultSslCert + ")")).toString();
+        if (!sslCert) {
+            sslCert = defaultSslCert;
+        }
+        server.sslCert = sslCert;
+        // ssl ca
+        let sslCa = (yield Cli_1.default.in("Q. Specify if there is a server intermediate certificate (CA). ()")).toString();
+        server.sslCa = [];
+        if (sslCa) {
+            server.sslCa.push(sslCa);
+        }
+        Cli_1.default.indent(2);
+    }
+    // port number
+    let defaultPort = 80;
+    if (server.ssl) {
+        defaultPort = 443;
+    }
+    let port = parseInt((yield Cli_1.default.in("Q. Specify port number. (" + defaultPort + ")")).toString(), 10);
+    if (!port) {
+        port = defaultPort;
+    }
+    server.port = port;
+    Cli_1.default.br().outData({
+        "Server Name": server.name,
+        "Host (Domain)": server.host,
+        "SSL": server.ssl,
+        "SSL Key": server.sslKey ? server.sslKey : "",
+        "SSL Cert": server.sslCert ? server.sslCert : "",
+        "SSL CA": server.sslCa ? server.sslCa : "",
+        "Port Number": server.port,
+    }).br();
+    const juge = yield Cli_1.default.in("Create a server with the above contents. Is it OK? [y/n] (y)");
+    if (!juge ||
+        juge == "y" ||
+        juge == "y") {
+    }
+    else {
+        Cli_1.default.br().redn("...Server Create Cancel!");
+    }
+});
--- cmd/create/index.ts
+++ cmd/create/index.ts
@@ -1,7 +1,125 @@
 import FlagCLI from "@flagfw/flag/bin/Cli";
+import ServerInit from "@flagfw/server/bin/common/ServerInit";
+import ServerUtil from "@flagfw/server/bin/common/Util";
 
-export default ()=>{
+export default async ()=>{
 
-    FlagCLI.outn("create server");
+    FlagCLI.outn("create server").br().indent(2);
+
+    let server : ServerInit = {};
+
+    let name;
+    while(!name){
+
+        name = (await FlagCLI.in("Q. Enter server name.")).toString();
+
+        if(!name){
+            FlagCLI.red("[ERROR] ").outn("No server name was entered. Please retry.");
+            continue;
+        }
+
+        const already = ServerUtil.getServer(name);
+
+        if(already){
+            FlagCLI.red("[ERROR] ").outn("The same server name already exists. Please specify a different name.");
+            name = null;
+            continue;
+        }
+    }
+
+    server.name = name;
+
+    // host name
+    let host = (await FlagCLI.in("Q. Enter host name (domain). (*)")).toString();
+
+    if(!host){
+        host = "*";
+    }
+
+    server.host = host;
+
+    // ssl
+    let ssl = await FlagCLI.in("Q. Do you want to make an SSL connection? [y/n] (y)");
+
+    server.ssl = false;
+    if(
+        !ssl ||
+        ssl == "y" ||
+        ssl == "Y"
+    ){
+        server.ssl = true;
+    }
+
+    if(server.ssl){
+
+        FlagCLI.indent(4);
+
+        // ssl key
+        const defaultSslKey = ServerUtil.getHome() + "/" + server.name + "/server.key";
+        let sslKey = (await FlagCLI.in("Q. Specify the private key path of the server certificate. (" + defaultSslKey + ")")).toString();
+
+        if(!sslKey){
+            sslKey = defaultSslKey;
+        }
+
+        server.sslKey = sslKey;
+
+        // ssl cert
+        const defaultSslCert = ServerUtil.getHome() + "/" + server.name + "/server.crt";
+        let sslCert = (await FlagCLI.in("Q. Specify server certificate path. (" + defaultSslCert + ")")).toString();
+
+        if(!sslCert){
+            sslCert = defaultSslCert;
+        }
     
+        server.sslCert = sslCert;
+
+        // ssl ca
+        let sslCa = (await FlagCLI.in("Q. Specify if there is a server intermediate certificate (CA). ()")).toString();
+        server.sslCa = [];
+        if(sslCa){
+            server.sslCa.push(sslCa);
+        }
+        
+        FlagCLI.indent(2);
+    }
+
+    // port number
+    let defaultPort = 80;
+    if(server.ssl){
+        defaultPort = 443;
+    }
+    let port = parseInt((await FlagCLI.in("Q. Specify port number. (" + defaultPort + ")")).toString(), 10);
+
+    if(!port){
+        port = defaultPort;
+    }
+
+    server.port = port;
+
+    FlagCLI.br().outData({
+        "Server Name": server.name,
+        "Host (Domain)": server.host,
+        "SSL" : server.ssl,
+        "SSL Key" : server.sslKey ? server.sslKey : "",
+        "SSL Cert": server.sslCert ? server.sslCert : "",
+        "SSL CA" : server.sslCa ? server.sslCa : "",
+        "Port Number" : server.port,
+    }).br();
+
+    const juge = await FlagCLI.in("Create a server with the above contents. Is it OK? [y/n] (y)");
+
+    if(
+        !juge ||
+        juge == "y" ||
+        juge == "y"
+    ){
+
+
+
+    }
+    else{
+        FlagCLI.br().redn("...Server Create Cancel!");
+    }
+
 };
