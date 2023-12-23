a83e98d95b1ff388e6c4ac2625d9871525a9ee79
masato nakatsuji <nakatsuji.sak19@gmail.com>
2023-12-23 18:19:01 +0900

    12/23

--- cmd/begin/thread.js
+++ cmd/begin/thread.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const os = require("os");
 const cluster = require("cluster");
@@ -8,24 +17,51 @@ const http_1 = require("./http");
 const https_1 = require("./https");
 // @ts-ignore
 if (cluster.isPrimary) {
-    const setting = Util_1.default.getSetting();
-    let threadLength = os.cpus().length;
-    if (setting) {
-        if (setting.thread) {
-            threadLength = setting.thread;
+    let workers = [];
+    const loadCluster = () => {
+        const setting = Util_1.default.getSetting();
+        let threadLength = os.cpus().length;
+        if (setting) {
+            if (setting.thread) {
+                threadLength = setting.thread;
+            }
         }
-    }
-    Cli_1.default.outn("Thread = " + threadLength);
-    for (let n = 0; n < threadLength; n++) {
-        // @ts-ignore
-        cluster.setupPrimary({
-            exec: __dirname + "/thread.js",
-            args: ["-number", n.toString()],
-        });
-        // @ts-ignore
-        const c = cluster.fork();
-        Cli_1.default.outn("Listen Thread(" + n + ") PID = " + c.process.pid);
-    }
+        Cli_1.default.outn("Thread = " + threadLength);
+        for (let n = 0; n < threadLength; n++) {
+            // @ts-ignore
+            cluster.setupPrimary({
+                exec: __dirname + "/thread.js",
+                args: ["-number", n.toString()],
+            });
+            // @ts-ignore
+            const c = cluster.fork();
+            Cli_1.default.outn("Listen Thread(" + n + ") PID = " + c.process.pid);
+            workers.push(c);
+        }
+        (() => __awaiter(void 0, void 0, void 0, function* () {
+            Cli_1.default.br();
+            let cmd;
+            while (!cmd) {
+                cmd = yield Cli_1.default.in(" > ");
+                if (cmd == "exit") {
+                    Cli_1.default.outn("....Exit");
+                    process.exit();
+                }
+                else if (cmd == "reload") {
+                    for (let n = 0; n < workers.length; n++) {
+                        const worker = workers[n];
+                        worker.kill();
+                    }
+                    Cli_1.default.outn("....Reload").br();
+                    loadCluster();
+                }
+                else {
+                    cmd = null;
+                }
+            }
+        }))();
+    };
+    loadCluster();
 }
 else {
     (0, http_1.default)();
--- cmd/begin/thread.ts
+++ cmd/begin/thread.ts
@@ -8,27 +8,62 @@ import https from "./https";
 // @ts-ignore
 if(cluster.isPrimary){
 
-    const setting = ServerUtil.getSetting();
+    let workers = [];
+    const loadCluster = ()=>{
 
-    let threadLength = os.cpus().length;
-    if(setting){
-        if(setting.thread){
-            threadLength = setting.thread;
+        const setting = ServerUtil.getSetting();
+
+        let threadLength = os.cpus().length;
+        if(setting){
+            if(setting.thread){
+                threadLength = setting.thread;
+            }
         }
-    }
 
-    FlagCLI.outn("Thread = " + threadLength);
+        FlagCLI.outn("Thread = " + threadLength);
 
-    for(let n = 0 ; n < threadLength ; n++){
-        // @ts-ignore
-        cluster.setupPrimary({
-            exec: __dirname + "/thread.js",
-            args: ["-number", n.toString()],
-        });
-        // @ts-ignore
-        const c = cluster.fork();
-        FlagCLI.outn("Listen Thread(" + n + ") PID = " + c.process.pid);
-    }
+        for(let n = 0 ; n < threadLength ; n++){
+            // @ts-ignore
+            cluster.setupPrimary({
+                exec: __dirname + "/thread.js",
+                args: ["-number", n.toString()],
+            });
+            // @ts-ignore
+            const c = cluster.fork();
+            FlagCLI.outn("Listen Thread(" + n + ") PID = " + c.process.pid);
+            workers.push(c);
+        }
+
+        (async()=>{
+
+            FlagCLI.br();
+    
+            let cmd;
+            while(!cmd){
+                cmd = await FlagCLI.in(" > ");
+    
+                if(cmd == "exit"){
+                    FlagCLI.outn("....Exit");
+                    process.exit();
+                }
+                else if(cmd == "reload"){
+                    for(let n = 0 ; n < workers.length ; n++){
+                        const worker = workers[n];
+                        worker.kill();
+                    }
+                    FlagCLI.outn("....Reload").br();
+                    loadCluster();
+                }
+                else{
+                    cmd = null;
+                }           
+            }
+    
+        })();
+    };
+    
+    loadCluster();    
+
 }
 else{
     http();
